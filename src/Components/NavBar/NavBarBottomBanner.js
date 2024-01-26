2d2719ea0c530ec83edff3d8baf0b9fa12f3f99b
Nelani Maluka <malukanelani@gmail.com>
2024-01-26 22:04:36 +0200

    added the bootom nav and its css

---
+++ src/Components/NavBar/NavBarBottomBanner.js
@@ -0,0 +1,13 @@
+import "./NavBarBottomBanner.css";
+
+function NavBarBottomBanner() {
+  return (
+    <div class="banner">
+      <p>
+        SHOP ALL SNEAKERS <a href="../shop?item=showAll">Shop</a>
+      </p>
+    </div>
+  );
+}
+
+export default NavBarBottomBanner;
