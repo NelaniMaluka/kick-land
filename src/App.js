8b6856d57d5300231d8862c2dac6e0b9ed6c378d
Nelani Maluka <malukanelani@gmail.com>
2024-01-27 20:23:06 +0200

    added the footer and home page to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
+import HomePage from "./Components/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <HomePage />
       <Footer />
     </div>
   );
