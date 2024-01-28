a1c6365aaaff696f07579f54873afb1a1d3706cf
Nelani Maluka <malukanelani@gmail.com>
2024-01-28 18:00:07 +0200

    added browser router and its routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
 import HomePage from "./Components/HomePage/HomePage";
+import Shop from "./Components/Shop/Shop";
+import Info from "./Components/Info/Info";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <HomePage />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Shop/:item" element={<Shop />} />
+          <Route path="/Info/:page" element={<Info />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
