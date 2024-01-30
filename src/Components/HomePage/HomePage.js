350d3ef1ea615694fc532f2f086d44a28d4622ca
Nelani Maluka <malukanelani@gmail.com>
2024-01-30 21:49:27 +0200

    added the banner images and its view

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,11 +1,14 @@
 import HomePageView1 from "./HomePageView1";
+import HomePageView2 from "./HomePageView2";
 import FooterBanner from "../Footer/FooterBanner";
 
 function HomePage() {
   return (
     <div>
       <HomePageView1 item="Dunk" />
-      <HomePageView1 item="Air-Force" />
+      <HomePageView2 item1="AirMax" item2="Jordan" />
+      <HomePageView1 item="AirForce" />
+      <HomePageView2 item1="Jordan" item2="AirMax" />
       <FooterBanner />
     </div>
   );
--- src/Components/HomePage/HomePageView2.jsx
+++ src/Components/HomePage/HomePageView2.jsx
@@ -1,5 +1,21 @@
-function HomePageView1() {
-  return <div></div>;
+import { Link } from "react-router-dom";
+import "./HomePage.css";
+
+function HomePageView2(item1, item2) {
+  return (
+    <div className="view2 container2">
+      <div className="banner-img">
+        <Link to={`./shop/${item1.item1}`}>
+          <img src={`/Images/HomeImages/banner-${item1.item1}-1.jpg`}></img>
+        </Link>
+      </div>
+      <div className="banner-img">
+        <Link to={`./shop/${item1.item2}`}>
+          <img src={`/Images/HomeImages/banner-${item1.item2}-2.jpg`}></img>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default HomePageView1;
+export default HomePageView2;
