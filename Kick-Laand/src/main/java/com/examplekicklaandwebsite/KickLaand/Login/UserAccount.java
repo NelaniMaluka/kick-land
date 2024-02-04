659e4aaf806b4b05e45c9efa77605a63c55649a5
Nelani Maluka <malukanelani@gmail.com>
2024-02-04 18:37:49 +0200

    removed notblank validations for userAccount

--- Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/Login/UserAccount.java
+++ Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/Login/UserAccount.java
@@ -4,13 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
-import com.examplekicklaandwebsite.KickLaand.Newsletter.Newsletter;
-
- 
 
 @Entity
 public class UserAccount {
@@ -19,15 +16,15 @@ public class UserAccount {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public int id;
 	
-	@NotBlank(message= "Please provide username")
 	public String username;
 	
-	@NotBlank(message= "Please provide Password")
 	public String surname;
 	
+	@Valid
 	@Email(message = "Please provide a valid email")
 	public String email;
 	
+	@Valid
 	@Size(min = 8, max=50, message = "Password must be at least 8 characters long")
 	public String password;
 
