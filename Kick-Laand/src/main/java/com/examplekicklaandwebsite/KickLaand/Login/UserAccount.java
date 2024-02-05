603d6f3aed0bc2193bcd77ff5161699694028f9c
Nelani Maluka <malukanelani@gmail.com>
2024-02-05 20:22:26 +0200

    added getters and setter for userAccount

--- Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/Login/UserAccount.java
+++ Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/Login/UserAccount.java
@@ -31,6 +31,42 @@ public class UserAccount {
 	public String getEmail() {
 		return email;
     }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
 	
