50523b8d07b1db4532a7ebda7fd2c88bf3ba9fda
Nelani Maluka <malukanelani@gmail.com>
2024-02-10 19:36:33 +0200

    added 3 more images for each product

--- Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/Products/Products.java
+++ Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/Products/Products.java
@@ -1,33 +1,33 @@
 package com.examplekicklaandwebsite.KickLaand.Products;
 
 import java.math.BigDecimal;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 @Entity
 public class Products {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    
-    @NotNull
-    private String name;
-    
-    @Pattern(regexp = "^[A-Z]{3}$", message = "Currency must be in uppercase and have three characters")
-    private BigDecimal price;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 	@NotNull
-    private String category;
-    
-    @NotNull
-    private String imageUrl;
+	private String name;
 
+	private BigDecimal price;
+	@NotNull
+	private String category;
+	private String image1;
+	private String image2;
+	private String image3;
+	private String image4;
+
+	public Products() {
+    }
+	
 	public int getId() {
 		return id;
 	}
@@ -44,6 +44,14 @@ public class Products {
 		this.name = name;
 	}
 
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
 	public String getCategory() {
 		return category;
 	}
@@ -51,23 +59,36 @@ public class Products {
 	public void setCategory(String category) {
 		this.category = category;
 	}
+
+	public String getImage1() {
+		return image1;
+	}
+
+	public void setImage1(String image1) {
+		this.image1 = image1;
+	}
+
+	public String getImage2() {
+		return image2;
+	}
+
+	public void setImage2(String image2) {
+		this.image2 = image2;
+	}
+
+	public String getImage3() {
+		return image3;
+	}
+
+	public void setImage3(String image3) {
+		this.image3 = image3;
+	}
 	
-	public BigDecimal getPrice() {
-		return price;
-	}
-	
-	public void setPrice(BigDecimal price) {
-		this.price = price;
+	public String getImage4() {
+		return image4;
 	}
 
-	public String getImageUrl() {
-		return imageUrl;
+	public void setImage4(String image4) {
+		this.image3 = image4;
 	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
-     
 }
-
--- Kick-Laand/src/main/resources/data.sql
+++ Kick-Laand/src/main/resources/data.sql
@@ -3,31 +3,40 @@ CREATE TABLE IF NOT EXISTS PRODUCTS (
     NAME VARCHAR(255) NOT NULL,
     PRICE DECIMAL(10,2) NOT NULL,
     CATEGORY VARCHAR(255) NOT NULL,
-    IMAGE_URL VARCHAR(255) NOT NULL
+    IMAGE1 VARCHAR(255),
+    IMAGE2 VARCHAR(255),
+    IMAGE3 VARCHAR(255),
+    IMAGE4 VARCHAR(255)
 );
 
-INSERT INTO PRODUCTS (CATEGORY, IMAGE_URL, NAME, PRICE)
+-- Dunk
+INSERT INTO PRODUCTS (CATEGORY, NAME, PRICE, IMAGE1, IMAGE2, IMAGE3, IMAGE4)
 VALUES
-  -- Dunk
-  ('Dunk', '/Images/ProductImages/Nike Dunk Low.png', 'Nike Dunk Low', 2099.95),
-  ('Dunk', '/Images/ProductImages/Nike Dunk Low Retro.png', 'Nike Dunk Low Retro', 2099.95),
-  ('Dunk', '/Images/ProductImages/Nike SB Dunk Low AA.png', 'Nike SB Dunk Low AA', 299.95),
-  ('Dunk', '/Images/ProductImages/Nike Dunk High Retro.png', 'Nike Dunk High Retro', 2399.95),
+  ('Dunk', 'Nike Dunk Low', 2099.95, '/Images/ProductImages/Nike Dunk Low.png', '/Images/ProductImages/Nike Dunk Low2.png', '/Images/ProductImages/Nike Dunk Low3.png', '/Images/ProductImages/Nike Dunk Low4.png'),
+  ('Dunk', 'Nike Dunk Low Retro', 2099.95, '/Images/ProductImages/Nike Dunk Low Retro.png', '/Images/ProductImages/Nike Dunk Low Retro2.png', '/Images/ProductImages/Nike Dunk Low Retro3.png', '/Images/ProductImages/Nike Dunk Low Retro4.png'),
+  ('Dunk', 'Nike SB Dunk Low AA', 299.95, '/Images/ProductImages/Nike SB Dunk Low AA.png', '/Images/ProductImages/Nike SB Dunk Low AA2.png', '/Images/ProductImages/Nike SB Dunk Low AA3.png', '/Images/ProductImages/Nike SB Dunk Low AA4.png'),
+  ('Dunk', 'Nike Dunk High Retro', 2399.95, '/Images/ProductImages/Nike Dunk High Retro.png', '/Images/ProductImages/Nike Dunk High Retro2.png', '/Images/ProductImages/Nike Dunk High Retro3.png', '/Images/ProductImages/Nike Dunk High Retro4.png');
 
-  -- Jordan
-  ('Jordan', '/Images/ProductImages/Air Jordan 1 Low.png', 'Air Jordan 1 Low', 1000),
-  ('Jordan', '/Images/ProductImages/Air Jordan 3 Retro.png', 'Air Jordan 3 Retro', 2000),
-  ('Jordan', '/Images/ProductImages/Air Jordan 1 Mid SE.png', 'Air Jordan 1 Mid SE', 3000),
-  ('Jordan', '/Images/ProductImages/Air Jordan Retro 13.png', 'Air Jordan Retro 13', 4000),
+-- Jordan
+INSERT INTO PRODUCTS (CATEGORY, NAME, PRICE, IMAGE1, IMAGE2, IMAGE3, IMAGE4)
+VALUES
+  ('Jordan', 'Air Jordan 1 Low', 1000, '/Images/ProductImages/Air Jordan 1 Low.png', '/Images/ProductImages/Air Jordan 1 Low2.png', '/Images/ProductImages/Air Jordan 1 Low3.png', '/Images/ProductImages/Air Jordan 1 Low4.png'),
+  ('Jordan', 'Air Jordan 3 Retro', 2000, '/Images/ProductImages/Air Jordan 3 Retro.png', '/Images/ProductImages/Air Jordan 3 Retro2.png', '/Images/ProductImages/Air Jordan 3 Retro3.png', '/Images/ProductImages/Air Jordan 3 Retro4.png'),
+  ('Jordan', 'Air Jordan 1 Mid SE', 3000, '/Images/ProductImages/Air Jordan 1 Mid SE.png', '/Images/ProductImages/Air Jordan 1 Mid SE2.png', '/Images/ProductImages/Air Jordan 1 Mid SE3.png', '/Images/ProductImages/Air Jordan 1 Mid SE4.png'),
+  ('Jordan', 'Air Jordan Retro 13', 4000, '/Images/ProductImages/Air Jordan Retro 13.png', '/Images/ProductImages/Air Jordan Retro 132.png', '/Images/ProductImages/Air Jordan Retro 133.png', '/Images/ProductImages/Air Jordan Retro 134.png');
 
-  -- AirForce
-  ('AirForce', '/Images/ProductImages/Nike Air Force 1 07.png', 'Nike Air Force 1 07', 1899.95),
-  ('AirForce', '/Images/ProductImages/Nike Air Force 1 LV8.png', 'Nike Air Force 1 LV8', 1899.95),
-  ('AirForce', '/Images/ProductImages/Nike Air Max 1 07 LX.png', 'Nike Air Max 1 07 LX', 2599.95),
-  ('AirForce', '/Images/ProductImages/Nike Air Max 1 07 WB.png', 'Nike Air Max 1 07 WB', 4000),
+-- AirForce
+INSERT INTO PRODUCTS (CATEGORY, NAME, PRICE, IMAGE1, IMAGE2, IMAGE3, IMAGE4)
+VALUES
+  ('AirForce', 'Nike Air Force 1 07', 1899.95, '/Images/ProductImages/Nike Air Force 1 07.png', '/Images/ProductImages/Nike Air Force 1 072.png', '/Images/ProductImages/Nike Air Force 1 073.png', '/Images/ProductImages/Nike Air Force 1 074.png'),
+  ('AirForce', 'Nike Air Force 1 LV8', 1899.95, '/Images/ProductImages/Nike Air Force 1 LV8.png', '/Images/ProductImages/Nike Air Force 1 LV82.png', '/Images/ProductImages/Nike Air Force 1 LV83.png', '/Images/ProductImages/Nike Air Force 1 LV84.png'),
+  ('AirForce', 'Nike Air Force 1 Low By You', 2599.95, '/Images/ProductImages/Nike Air Force 1 Low By You.png', '/Images/ProductImages/Nike Air Force 1 Low By You2.png', '/Images/ProductImages/Nike Air Force 1 Low By You3.png', '/Images/ProductImages/Nike Air Force 1 Low By You4.png'),
+  ('AirForce', 'Nike Air Force 1 Luxe', 4000, '/Images/ProductImages/Nike Air Force 1 Luxe.png', '/Images/ProductImages/Nike Air Force 1 Luxe2.png', '/Images/ProductImages/Nike Air Force 1 Luxe3.png', '/Images/ProductImages/Nike Air Force 1 Luxe4.png');
 
-  -- AirMax
-  ('AirMax', '/Images/ProductImages/Nike Air Max Plus.png', 'Nike Air Max Plus', 3599.99),
-  ('AirMax', '/Images/ProductImages/Nike Air Max Pulse.png', 'Nike Air Max Pulse', 3599.95),
-  ('AirMax', '/Images/ProductImages/Nike Air Max 97.png', 'Nike Air Max 97', 2599.97),
-  ('AirMax', '/Images/ProductImages/Nike Air Max 1.png', 'Nike Air Max 1', 2799.95);
+-- AirMax
+INSERT INTO PRODUCTS (CATEGORY, NAME, PRICE, IMAGE1, IMAGE2, IMAGE3, IMAGE4)
+VALUES
+  ('AirMax', 'Nike Air Max Plus', 3599.99, '/Images/ProductImages/Nike Air Max Plus.png', '/Images/ProductImages/Nike Air Max Plus2.png', '/Images/ProductImages/Nike Air Max Plus3.png', '/Images/ProductImages/Nike Air Max Plus4.png'),
+  ('AirMax', 'Nike Air Max Pulse', 3599.95, '/Images/ProductImages/Nike Air Max Pulse.png', '/Images/ProductImages/Nike Air Max Pulse2.png', '/Images/ProductImages/Nike Air Max Pulse3.png', '/Images/ProductImages/Nike Air Max Pulse4.png'),
+  ('AirMax', 'Nike Air Max 97', 2599.97, '/Images/ProductImages/Nike Air Max 97.png', '/Images/ProductImages/Nike Air Max 972.png', '/Images/ProductImages/Nike Air Max 973.png', '/Images/ProductImages/Nike Air Max 974.png'),
+  ('AirMax', 'Nike Air Max 1', 2799.95, '/Images/ProductImages/Nike Air Max 1.png', '/Images/ProductImages/Nike Air Max 12.png', '/Images/ProductImages/Nike Air Max 13.png', '/Images/ProductImages/Nike Air Max 14.png');
