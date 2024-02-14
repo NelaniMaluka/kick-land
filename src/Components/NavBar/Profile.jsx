9d8aa86bf4e952682cc8406a97999a625d836741
Nelani Maluka <malukanelani@gmail.com>
2024-02-14 06:29:13 +0200

    added the profile icon

---
+++ src/Components/NavBar/Profile.jsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { useAuth } from "../Security/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+export default function Profile() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const authContext = useAuth();
+  const isAuthenticated = authContext.isAuthenticated;
+
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    authContext.logout();
+    navigate("/");
+  }
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton
+        size="large"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleMenu}
+        color="inherit"
+      >
+        <AccountCircle />
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link to="/profile" style={{ fontFamily: "arial" }}>
+            Profile
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          {!isAuthenticated && (
+            <li>
+              <Link to="/Login" style={{ fontFamily: "arial" }}>
+                Log In
+              </Link>
+            </li>
+          )}
+          {isAuthenticated && (
+            <li>
+              <Link onClick={handleLogout} style={{ fontFamily: "arial" }}>
+                Log Out
+              </Link>
+            </li>
+          )}
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
