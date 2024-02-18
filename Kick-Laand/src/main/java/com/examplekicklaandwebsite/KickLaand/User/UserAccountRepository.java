fa11e0ccbb49ecc02f83a1eb315cf129cad548d3
Nelani Maluka <malukanelani@gmail.com>
2024-02-18 09:04:32 +0200

    added findByEmail querry

--- Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/User/UserAccountRepository.java
+++ Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/User/UserAccountRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserAccountRepository extends JpaRepository<UserAccount,Integer>{
 	
 	UserAccount findByEmailAndPassword(String email, String password);
+	
+	UserAccount findByEmail(String email);
+	
 
 }
