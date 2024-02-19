0512fc036105e90af2d9f6dca829e2aecf251b19
Nelani Maluka <malukanelani@gmail.com>
2024-02-19 18:23:15 +0200

    removed useStates

--- src/Components/Dashboard/Profile/UserProfile.jsx
+++ src/Components/Dashboard/Profile/UserProfile.jsx
@@ -1,49 +1,35 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../../Security/AuthContext.js";
 import Swal from "sweetalert2";
+import { Formik } from "formik";
 import LocationInput from "./LocationInput.tsx";
-
 import "./Profile.css";
 
 function UserProfile() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [message, setMessage] = useState("");
   const [errormessage, setErrormessage] = useState("");
   const [ShowErrormessage, setShowErrormessage] = useState(false);
 
   const AuthContext = useAuth();
+  const user = AuthContext.isUser;
+  console.log(user);
 
-  function handleNameChange(event) {
-    setName(event.target.value);
-  }
+  useEffect(() => {
+    // No need to set initial form values using useEffect
+  }, [user]);
 
-  function handleEmailChange(event) {
-    setEmail(event.target.value);
-  }
-
-  function handlePhoneNumberChange(event) {
-    setPhoneNumber(event.target.value);
-  }
-
-  function handleMessageChange(event) {
-    setMessage(event.target.value);
-  }
-
-  function handleSubmit() {
-    AuthContext.ContactUs(name, email, phoneNumber, message)
+  function handleSubmit(values) {
+    AuthContext.updateUserDetails(
+      user.id,
+      values.name,
+      values.surname,
+      values.email,
+      values.phoneNumber
+    )
       .then(function (result) {
         if (result.success) {
-          // API call was successful, handle the success
-          showSuccessMessage(true);
+          showSuccessMessage(result);
           setShowErrormessage(false);
-          setName("");
-          setEmail("");
-          setPhoneNumber("");
-          setMessage("");
         } else {
-          // API call failed, handle the error
           setShowErrormessage(true);
           console.error("ContactUs failed:", result.error);
           setErrormessage(
@@ -52,7 +38,6 @@ function UserProfile() {
         }
       })
       .catch(function (error) {
-        // Handle other unexpected errors
         setShowErrormessage(true);
         console.error("Unexpected error in ContactUs:", error);
         setErrormessage("Unexpected error please ContactUs");
@@ -68,59 +53,72 @@ function UserProfile() {
   }
 
   return (
-    <form className=" profile">
-      <div>
-        <h2> Personal Information:</h2>
-      </div>
-      {ShowErrormessage && <div className="error">{errormessage}</div>}
-      <div className="field-1">
-        <input
-          className="messageField"
-          placeholder="Name"
-          type="name"
-          name="name"
-          value={name}
-          onChange={handleNameChange}
-        />
-        <input
-          className="messageField"
-          placeholder="Email"
-          type="email"
-          name="email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-      </div>
-      <div className="field-1">
-        <input
-          className="messageField"
-          placeholder="Phone Number"
-          type="phone"
-          name="phoneNumber"
-          value={phoneNumber}
-          onChange={handlePhoneNumberChange}
-        />
-        <input
-          className="messageField"
-          placeholder="Phone Number"
-          type="phone"
-          name="phoneNumber"
-          value={phoneNumber}
-          onChange={handlePhoneNumberChange}
-        />
-      </div>
-      <div>
-        <h2> Address Information:</h2>
-      </div>
-      <div>
-        <LocationInput />
-      </div>
-      <div>
-        <button type="button" name="login" onClick={handleSubmit}>
-          Save Changes
-        </button>
-      </div>
-    </form>
+    <Formik
+      initialValues={{
+        name: user.username || "",
+        surname: user.surname || "",
+        phoneNumber: user.phoneNumber || "",
+        email: user.email || "",
+      }}
+      onSubmit={handleSubmit}
+    >
+      {({ values, handleChange, handleSubmit }) => (
+        <form className="profile">
+          <div>
+            <h2> Personal Information:</h2>
+          </div>
+          {ShowErrormessage && <div className="error">{errormessage}</div>}
+          <div className="field-1">
+            <input
+              className="messageField"
+              placeholder="Name"
+              type="name"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+            />
+            <input
+              className="messageField"
+              placeholder="Surname"
+              type="Surname"
+              name="surname"
+              value={values.surname}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="field-1">
+            <input
+              className="messageField"
+              placeholder="Phone Number"
+              type="phone"
+              name="phoneNumber"
+              value={values.phoneNumber}
+              onChange={handleChange}
+            />
+            <input
+              className="messageField"
+              placeholder="Email"
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <h2> Address Information:</h2>
+          </div>
+          <div>
+            <LocationInput />
+          </div>
+          <div>
+            <button type="button" name="login" onClick={handleSubmit}>
+              Save Changes
+            </button>
+          </div>
+        </form>
+      )}
+    </Formik>
   );
 }
+
 export default UserProfile;
