dbf9e621f149517c286ba4ac1f3b56eed6af263a
Nelani Maluka <malukanelani@gmail.com>
2024-02-23 21:55:08 +0200

    added test annotation and a VaryingInputsWithWrongData function

--- Kick-Laand/src/test/java/com/examplekicklaandwebsite/KickLaand/ContactUs/ContactUsTest.java
+++ Kick-Laand/src/test/java/com/examplekicklaandwebsite/KickLaand/ContactUs/ContactUsTest.java
@@ -4,19 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
-import static org.mockito.ArgumentMatchers.anyString;
-
 import com.examplekicklaandwebsite.KickLaand.Newsletter.NewsletterRepository;
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class ContactUsTest {
 
 	@Mock
@@ -36,8 +35,8 @@ class ContactUsTest {
 	    "TestName, Testemail@gmail.com, null, This is a test",
 	    "TestName, Testemail@gmail.com, +278765432109, null",
 	})
-	void sendContactDetails_VaryingInputs(String name, String email, String phoneNumber, String message) {
-	    ContactUs contactUs = new ContactUs(1, name, email, phoneNumber, message);
+	void sendContactDetails_VaryingInputsWithNull(String name, String email, String phoneNumber, String message) {
+	    ContactUs contactUs = new ContactUs(name, email, phoneNumber, message);
 	    lenient().when(newsletterRepositoryMock.findByEmail(email)).thenReturn(null);
 	    ResponseEntity<?> responseEntity = contactUsController.sendInfo(contactUs);
 
@@ -49,8 +48,24 @@ class ContactUsTest {
 	        assertEquals(ResponseEntity.badRequest().body("Email, Phone Number, and Message are Required"), responseEntity);
 	    }
 	}
+	
+	@ParameterizedTest
+	@CsvSource({
+	    "TestName, w, +278765432109, This is a test",
+	    "TestName, Testemail@gmail.com, Wrong_Input, This is a test",
+	})
+	void sendContactDetails_VaryingInputsWithWrongData(String name, String email, String phoneNumber, String message) {
+	    ContactUs contactUs = new ContactUs(name, email, phoneNumber, message);
 
+	    // Mock behavior
+	    when(newsletterRepositoryMock.findByEmail(email)).thenReturn(null);
 
+	    // Call the method with the BindingResult parameter
+	    ResponseEntity<?> responseEntity = contactUsController.sendInfo(contactUs );
 
-
+	    System.out.println("Response Status: " + responseEntity.getStatusCode());
+	    System.out.println("Response Body: " + responseEntity.getBody());
+	    
+	    assertEquals(ResponseEntity.badRequest().body("Invalid Credentials"), responseEntity); 
+	}
 }
