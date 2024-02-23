0bbcdffcd3b53226674b5135767adbcb45c4961b
Nelani Maluka <malukanelani@gmail.com>
2024-02-23 21:50:26 +0200

    moved the repository save

--- Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/ContactUs/ContactUsController.java
+++ Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/ContactUs/ContactUsController.java
@@ -16,38 +16,38 @@ import com.examplekicklaandwebsite.KickLaand.Newsletter.NewsletterRepository;
 @RestController
 @Validated
 public class ContactUsController {
-	
+
 	private final ContactUsRepository contactUsRepository;
 	private final NewsletterRepository newsletterRepository;
-	
+
 	public ContactUsController(ContactUsRepository contactUsRepository, NewsletterRepository newsletterRepository) {
 		super();
 		this.contactUsRepository = contactUsRepository;
 		this.newsletterRepository = newsletterRepository;
 	}
 
-	@PostMapping(path="/Backend/ContactUs")
+	@PostMapping(path = "/Backend/ContactUs")
 	public ResponseEntity<?> sendInfo(@Valid @RequestBody ContactUs contactUs) {
-	    try {
-	        contactUsRepository.save(contactUs);
+		try {
+			// Check if email is not null before creating a Newsletter instance
+			if (contactUs.getEmail() != null && contactUs.getPhoneNumber() != null && contactUs.getMessage() != null) {
+				Newsletter newsletter = new Newsletter();
+				newsletter.setEmail(contactUs.getEmail());
 
-	        // Check if email is not null before creating a Newsletter instance
-	        if (contactUs.getEmail() != null && contactUs.getPhoneNumber() != null && contactUs.getMessage() != null) {
-	            Newsletter newsletter = new Newsletter();
-	            newsletter.setEmail(contactUs.getEmail());
+				// Assuming FindByEmail returns a nullable result
+				Optional<Newsletter> existingNewsletter = Optional
+						.ofNullable(newsletterRepository.findByEmail(contactUs.getEmail()));
 
-	            // Assuming FindByEmail returns a nullable result
-	            Optional<Newsletter> existingNewsletter = Optional.ofNullable(newsletterRepository.findByEmail(contactUs.getEmail()));
-	            
-	            if (!existingNewsletter.isPresent()) {
-	                newsletterRepository.save(newsletter);
-	            }
-	            return ResponseEntity.ok("We Received your message");
-	        } else {
-	        	return ResponseEntity.badRequest().body("Email, Phone Number and Message is Required");
-	        }
-	    } catch (Exception e) {
-	        return ResponseEntity.badRequest().body("Invalid Credentials");
-	    }
+				if (!existingNewsletter.isPresent()) {
+					newsletterRepository.save(newsletter);
+				}
+				contactUsRepository.save(contactUs);
+				return ResponseEntity.ok("We Received your message");
+			} else {
+				return ResponseEntity.badRequest().body("Email, Phone Number and Message is Required");
+			}
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body("Invalid Credentials");
+		}
 	}
 }
