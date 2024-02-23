b466874fa8bee715059406a6f52f354f41d45f34
Nelani Maluka <malukanelani@gmail.com>
2024-02-23 21:52:04 +0200

    removed unnesesary querries

--- Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/User/CartRepository.java
+++ Kick-Laand/src/main/java/com/examplekicklaandwebsite/KickLaand/User/CartRepository.java
@@ -1,13 +1,7 @@
 package com.examplekicklaandwebsite.KickLaand.User;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
-
-    List<Cart> findByUser(UserAccount user);
-    
-    //Cart findByProductidandUser(Long id);
 
 }
