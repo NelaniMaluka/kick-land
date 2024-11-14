f44b1bc5bfc307f517903ed043be8b8308b1572f
Nelani Maluka <malukanelani@gmail.com>
2024-11-14 16:44:48 +0200

    change

--- Frontend/src/Components/NavBar/NavBarMain.jsx
+++ Frontend/src/Components/NavBar/NavBarMain.jsx
@@ -17,7 +17,7 @@ function NavBarMain() {
     <nav>
       <div className="navC container2">
         <Link to="/" className="name  ">
-          Kick Land
+          Kicks Land
         </Link>
         <ul className="sneakers">
           <li>
