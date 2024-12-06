6f410c2f93f30ec0cf754fe03ea968a6ab2ba62e
Nelani Maluka <malukanelani@gmail.com>
2024-12-06 09:54:04 +0200

    restructured api calls and mappings

--- Frontend/src/Context/Api.js
+++ Frontend/src/Context/Api.js
@@ -4,13 +4,7 @@ const apiClient = axios.create({
   baseURL: "http://localhost:8080",
 });
 
-export function LogIn(email, password) {
-  return apiClient.post("/api/user/login", {
-    email,
-    password,
-  });
-}
-
+// User API Calls
 export function CreateAccount(firstname, lastname, email, password) {
   return apiClient.post("/api/user/create-account", {
     firstname,
@@ -19,63 +13,12 @@ export function CreateAccount(firstname, lastname, email, password) {
     password,
   });
 }
-
-export function SignForNewsletter(email) {
-  return apiClient.post("/api/public/newsletter", { email });
-}
-
-export function ContactUs(name, email, phoneNumber, message) {
-  return apiClient.post("/api/public/contactUs", {
-    name,
+export function LogIn(email, password) {
+  return apiClient.post("/api/user/login", {
     email,
-    phoneNumber,
-    message,
+    password,
   });
 }
-
-export function RetrieveProducts() {
-  return apiClient.get("/api/public/products");
-}
-
-export function GetUserCart(email) {
-  return apiClient.get(`/api/user/cart?email=${email}`);
-}
-
-export function AddToCart(cartProduct) {
-  return apiClient.post("/api/user/cart", cartProduct);
-}
-
-export function DeleteCartItem(userId, productId) {
-  const parsedUserId = parseInt(userId, 10);
-  const parsedProductId = parseInt(productId, 10);
-
-  return apiClient.delete(
-    `/api/user/cart?userId=${parsedUserId}&productId=${parsedProductId}`
-  );
-}
-
-export function UpdateCartItem(userId, productId, productQuantity) {
-  const parsedUserId = parseInt(userId, 10);
-  const parsedProductId = parseInt(productId, 10);
-  const parsedproductQuantity = parseInt(productQuantity, 10);
-
-  return apiClient.put(
-    `/api/user/cart?userId=${parsedUserId}&productId=${parsedProductId}&productQuantity=${parsedproductQuantity}`
-  );
-}
-
-export function GetUserOrders(email) {
-  return apiClient.get(`/api/user/orders?email=${email}`);
-}
-
-export function AddToOrders(productWithUserId) {
-  return apiClient.post("/api/user/orders", productWithUserId);
-}
-
-export function ForgotPassword(email) {
-  return apiClient.post("/api/user/forgot-password", { email });
-}
-
 export function UpdateUserDetails(
   userId,
   firstname,
@@ -90,5 +33,74 @@ export function UpdateUserDetails(
     email,
     phonenumber,
     address,
+  });
+}
+
+//User Cart API calls
+export function GetUserCart(email) {
+  return apiClient.get(`/api/cart?email=${email}`);
+}
+export function AddToCart(cartProduct) {
+  return apiClient.post("/api/cart", cartProduct);
+}
+export function UpdateCartItem(userId, productId, productQuantity) {
+  const parsedUserId = parseInt(userId, 10);
+  const parsedProductId = parseInt(productId, 10);
+  const parsedproductQuantity = parseInt(productQuantity, 10);
+
+  return apiClient.put(
+    `/api/cart?userId=${parsedUserId}&productId=${parsedProductId}&productQuantity=${parsedproductQuantity}`
+  );
+}
+export function DeleteCartItem(userId, productId) {
+  const parsedUserId = parseInt(userId, 10);
+  const parsedProductId = parseInt(productId, 10);
+
+  return apiClient.delete(
+    `/api/cart?userId=${parsedUserId}&productId=${parsedProductId}`
+  );
+}
+
+// Newsletter API call
+export function SignForNewsletter(email) {
+  return apiClient.post("/api/newsletter", { email });
+}
+
+//Contact Us API call
+export function ContactUs(name, email, phoneNumber, message) {
+  return apiClient.post("/api/contact-us", {
+    name,
+    email,
+    phoneNumber,
+    message,
+  });
+}
+
+//Products API Call
+export function RetrieveProducts() {
+  return apiClient.get("/api/products");
+}
+
+//User Orders API calls
+export function GetUserOrders(email) {
+  return apiClient.get(`/api/orders?email=${email}`);
+}
+export function AddToOrders(productWithUserId) {
+  return apiClient.post("/api/orders", productWithUserId);
+}
+
+//Forgot Password API calls
+export function ForgotPassword(email) {
+  return apiClient.post("/api/forgot-password/verify-mail", { email });
+}
+export function VerifyOtp(otp, email) {
+  return apiClient.post("/api/forgot-password/verify-otp", { otp, email });
+}
+
+export function ChangePassword(password, repeatPassword, email) {
+  return apiClient.post("/api/forgot-password/change-password", {
+    password,
+    repeatPassword,
+    email,
   });
 }
--- Frontend/src/Context/AuthContext.js
+++ Frontend/src/Context/AuthContext.js
@@ -13,6 +13,8 @@ import {
   ForgotPassword,
   LogIn,
   CreateAccount,
+  VerifyOtp,
+  ChangePassword,
 } from "./Api";
 import { useEffect } from "react";
 
@@ -26,7 +28,28 @@ function AuthProvider({ children }) {
   const [isProducts, setProducts] = useState([]);
   const [isCartItems, setCartItems] = useState([]);
   const [isUserOrders, setUserOrders] = useState([]);
+  const [isForgotPasswordEmail, setForgotPasswordEmail] = useState([]);
 
+  useEffect(() => {
+    // Fetch products only when the component mounts
+    retrieveProducts();
+  }, []);
+
+  //retrieve products function
+  async function retrieveProducts() {
+    try {
+      const response = await RetrieveProducts();
+      if (response.status === 200) {
+        setProducts(response.data);
+        console.log(response.data);
+      } else {
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  //User API methods
   async function login(email, password) {
     try {
       const response = await LogIn(email, password);
@@ -45,13 +68,11 @@ function AuthProvider({ children }) {
       return { success: false, response: e.response };
     }
   }
-
   async function logout() {
     setAuthenticated(false);
     setUser(null);
     setCartItems([]);
   }
-
   async function createAccount(firstname, lastname, email, password) {
     try {
       const response = await CreateAccount(
@@ -73,128 +94,6 @@ function AuthProvider({ children }) {
       return { success: false, response: e.response };
     }
   }
-
-  async function signUpForNewsletter(email) {
-    try {
-      const response = await SignForNewsletter(email);
-      return { success: response.status === 200, response };
-    } catch (e) {
-      return { success: false, response: e.response };
-    }
-  }
-
-  async function contactUs(name, email, phoneNumber, message) {
-    try {
-      const response = await ContactUs(name, email, phoneNumber, message);
-      return { success: response.status === 200, response };
-    } catch (e) {
-      return { success: false, response: e.response };
-    }
-  }
-
-  useEffect(() => {
-    // Fetch products only when the component mounts
-    retrieveProducts();
-  }, []);
-
-  async function retrieveProducts() {
-    try {
-      const response = await RetrieveProducts();
-      if (response.status === 200) {
-        setProducts(response.data);
-      } else {
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  async function getUserCart(email) {
-    try {
-      const response = await GetUserCart(email);
-      if (response.status === 200) {
-        setCartItems(response.data);
-      } else {
-        setCartItems(null);
-      }
-    } catch (e) {
-      setCartItems(null);
-    }
-  }
-
-  async function addToCart(productWithUserId) {
-    try {
-      const response = await AddToCart(productWithUserId);
-      if (response.status === 200) {
-        setCartItems(response.data);
-        return true;
-      } else {
-        setCartItems([]);
-        return false;
-      }
-    } catch (e) {
-      setCartItems([]);
-    }
-  }
-
-  async function updateCartItem(userId, productId, productQuantity) {
-    try {
-      const response = await UpdateCartItem(userId, productId, productQuantity);
-      console.log(response);
-      if (response.status === 200) {
-        setCartItems(response.data);
-        return { success: true, response: response };
-      } else {
-        return response;
-      }
-    } catch (e) {
-      return e;
-    }
-  }
-
-  async function deleteCartItem(userId, productId) {
-    try {
-      const response = await DeleteCartItem(userId, productId);
-      if (response.status === 200) {
-        setCartItems(response.data);
-        return { success: true, response: response };
-      } else {
-        return response;
-      }
-    } catch (e) {
-      return e;
-    }
-  }
-
-  async function getUserOrders(email) {
-    try {
-      const response = await GetUserOrders(email);
-      if (response.status === 200) {
-        setUserOrders(response.data);
-      } else {
-        setUserOrders(null);
-      }
-    } catch (e) {
-      setUserOrders(null);
-    }
-  }
-
-  async function addToOrders(orders) {
-    console.log(orders);
-    try {
-      const response = await AddToOrders(orders);
-      if (response.status === 200) {
-        setUserOrders(response.data);
-        return true;
-      } else {
-        setUserOrders([]);
-        return false;
-      }
-    } catch (e) {
-      setUserOrders([]);
-    }
-  }
-
   async function updateUserDetails(
     userId,
     firstname,
@@ -225,9 +124,145 @@ function AuthProvider({ children }) {
     }
   }
 
+  // Newsletter API method
+  async function signUpForNewsletter(email) {
+    try {
+      const response = await SignForNewsletter(email);
+      return { success: response.status === 200, response };
+    } catch (e) {
+      return { success: false, response: e.response };
+    }
+  }
+
+  //Contact Us API method
+  async function contactUs(name, email, phoneNumber, message) {
+    try {
+      const response = await ContactUs(name, email, phoneNumber, message);
+      return { success: response.status === 200, response };
+    } catch (e) {
+      return { success: false, response: e.response };
+    }
+  }
+
+  //User Cart API methods
+  async function getUserCart(email) {
+    try {
+      const response = await GetUserCart(email);
+      if (response.status === 200) {
+        setCartItems(response.data);
+      } else {
+        setCartItems(null);
+      }
+    } catch (e) {
+      setCartItems(null);
+    }
+  }
+  async function addToCart(productWithUserId) {
+    try {
+      const response = await AddToCart(productWithUserId);
+      if (response.status === 200) {
+        setCartItems(response.data);
+        return true;
+      } else {
+        setCartItems([]);
+        return false;
+      }
+    } catch (e) {
+      setCartItems([]);
+    }
+  }
+  async function updateCartItem(userId, productId, productQuantity) {
+    try {
+      const response = await UpdateCartItem(userId, productId, productQuantity);
+      console.log(response);
+      if (response.status === 200) {
+        setCartItems(response.data);
+        return { success: true, response: response };
+      } else {
+        return response;
+      }
+    } catch (e) {
+      return e;
+    }
+  }
+  async function deleteCartItem(userId, productId) {
+    try {
+      const response = await DeleteCartItem(userId, productId);
+      if (response.status === 200) {
+        setCartItems(response.data);
+        return { success: true, response: response };
+      } else {
+        return response;
+      }
+    } catch (e) {
+      return e;
+    }
+  }
+
+  //User Orders API methods
+  async function getUserOrders(email) {
+    try {
+      const response = await GetUserOrders(email);
+      if (response.status === 200) {
+        setUserOrders(response.data);
+      } else {
+        setUserOrders(null);
+      }
+    } catch (e) {
+      setUserOrders(null);
+    }
+  }
+  async function addToOrders(orders) {
+    console.log(orders);
+    try {
+      const response = await AddToOrders(orders);
+      if (response.status === 200) {
+        setUserOrders(response.data);
+        return true;
+      } else {
+        setUserOrders([]);
+        return false;
+      }
+    } catch (e) {
+      setUserOrders([]);
+    }
+  }
+
+  //Forgot Password API methods
   async function forgotPassword(email) {
     try {
       const response = await ForgotPassword(email);
+      console.log(response);
+      if (response.status === 200) {
+        setForgotPasswordEmail(email);
+        return { success: true, response };
+      } else {
+        return { success: false, response };
+      }
+    } catch (e) {
+      return { success: false, e };
+    }
+  }
+  async function verifyOtp(otp) {
+    try {
+      const response = await VerifyOtp(otp, isForgotPasswordEmail);
+      if (response.status === 200) {
+        return { success: true, response };
+      } else {
+        return { success: false, response };
+      }
+    } catch (e) {
+      return { success: false, e };
+    }
+  }
+  async function changePassword(password, repeatPassword) {
+    try {
+      const response = await ChangePassword(
+        password,
+        repeatPassword,
+        isForgotPasswordEmail
+      );
+      console.log(response);
       if (response.status === 200) {
         return { success: true, response };
       } else {
@@ -258,6 +293,8 @@ function AuthProvider({ children }) {
         isUserOrders,
         addToOrders,
         forgotPassword,
+        verifyOtp,
+        changePassword,
       }}
     >
       {children}
