2ca6823cf32911e513a50c4b8667759781302a1f
Nelani Maluka <malukanelani@gmail.com>
2025-01-12 18:03:17 +0200

    added orders view

---
+++ Frontend/src/Pages/Dashboard/Order/OrdersView.jsx
@@ -0,0 +1,39 @@
+import { useSelector } from "react-redux";
+import moment from "moment";
+
+const OrdersView = () => {
+  const orders = useSelector((state) => state.order.order);
+  console.log(orders);
+
+  if (orders.length === 0) {
+    return (
+      <div className="no-cart-items">
+        <span>
+          <span class="material-symbols-outlined">playlist_add</span> No cart
+          items available.
+        </span>
+      </div>
+    );
+  }
+
+  const formatDate = (date) => moment(date).format("MMMM D, YYYY");
+
+  return (
+    <div className="parent-container">
+      <div className="order-container">
+        {orders.map((orderItem) => {
+          return (
+            <div className="cart-card" key={orderItem.orderId}>
+              <span>Order Date: {formatDate(orderItem.orderDate)}</span>
+              <span>Order: #{orderItem.orderId}</span>
+              <span>Delivery Date: {formatDate(orderItem.deliveryDate)}</span>
+              <span>Shipping To: {formatDate(orderItem.deliveryDate)}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default OrdersView;
