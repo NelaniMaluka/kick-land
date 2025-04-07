5a0b9a4ae8a3875856f30bec45ad52493ffaf95c
Nelani Maluka <malukanelani@gmail.com>
2025-04-07 05:10:14 -0400

    chore: added comments and loading screen

--- Frontend/src/Pages/Shop/ShopAll.jsx
+++ Frontend/src/Pages/Shop/ShopAll.jsx
@@ -3,11 +3,15 @@ import ShopProductBanner from "../../Components/Banners/ShopProductBanner";
 import ShopProductsCard from "../../Components/Cards/ShopProductsCard";
 import { useState, useEffect } from "react";
 import AssuranceBanner from "../../Components/Banners/AssuranceBanner";
+import ErrorMessageAlert from "../../Components/Alerts/ErrorMessageAlert";
+import CircularIndeterminate from "../../Utils/LoadingSpinner";
 
 export default function ShopAll() {
   const authContext = useAuth();
+  const [loading, setLoading] = useState(true);
   const [isProducts, setProducts] = useState(authContext.isProducts || []);
 
+  // Fetsches Product data on page load
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -22,6 +26,30 @@ export default function ShopAll() {
 
     fetchData();
   }, [authContext, isProducts.length]);
+
+  // Handles loading screen
+  useEffect(() => {
+    if (!isProducts || isProducts.length === 0) {
+      setLoading(true);
+      return;
+    }
+
+    try {
+      if (isProducts || isProducts.length >= 1) {
+        setLoading(false);
+        return;
+      }
+    } catch (err) {
+      ErrorMessageAlert({
+        message:
+          "We're sorry, something went wrong while loading products. Please try refreshing the page or check back later.",
+      });
+      setLoading(false);
+    }
+  }, [isProducts]);
+
+  // Returs the loading screen
+  if (loading) return <CircularIndeterminate />;
 
   return (
     <>
--- Frontend/src/Pages/Shop/ShopByCategory.jsx
+++ Frontend/src/Pages/Shop/ShopByCategory.jsx
@@ -4,13 +4,17 @@ import ShopProductsCard from "../../Components/Cards/ShopProductsCard";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import AssuranceBanner from "../../Components/Banners/AssuranceBanner";
+import ErrorMessageAlert from "../../Components/Alerts/ErrorMessageAlert";
+import CircularIndeterminate from "../../Utils/LoadingSpinner";
 
 export default function ShopByCategory() {
   const authContext = useAuth();
+  const [loading, setLoading] = useState(true);
   const [isProducts, setProducts] = useState(authContext.isProducts || []);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const category = useParams();
 
+  // Fetches the products on page load
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -26,11 +30,36 @@ export default function ShopByCategory() {
     fetchData();
   }, [authContext, isProducts.length]);
 
+  // Sets  the filtered products
   useEffect(() => {
     setFilteredProducts(
       isProducts.filter((product) => product.category === category.item)
     );
   }, [isProducts, category]);
+
+  // Handles loading screen
+  useEffect(() => {
+    if (!isProducts || isProducts.length === 0) {
+      setLoading(true);
+      return;
+    }
+
+    try {
+      if (isProducts || isProducts.length >= 1) {
+        setLoading(false);
+        return;
+      }
+    } catch (err) {
+      ErrorMessageAlert({
+        message:
+          "We're sorry, something went wrong while loading products. Please try refreshing the page or check back later.",
+      });
+      setLoading(false);
+    }
+  }, [isProducts]);
+
+  // Returs the loading screen
+  if (loading) return <CircularIndeterminate />;
 
   return (
     <>
