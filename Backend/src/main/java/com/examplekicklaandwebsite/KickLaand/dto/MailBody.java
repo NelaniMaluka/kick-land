2a700b1d5b77c1126d8b13e74099459ccc7d748e
Nelani Maluka <malukanelani@gmail.com>
2025-05-24 11:17:36 +0200

    fix: changed mailbody from a recordinto a class

--- Backend/src/main/java/com/examplekicklaandwebsite/KickLaand/dto/MailBody.java
+++ Backend/src/main/java/com/examplekicklaandwebsite/KickLaand/dto/MailBody.java
@@ -2,11 +2,19 @@ package com.examplekicklaandwebsite.KickLaand.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.Data;
 
+@Data
 @Builder
-public record MailBody(
-        @NotNull(message = "Recipient Email Address cannot be null")String to,
-        @NotNull(message = "Email Subject cannot be null")String subject,
-        @NotNull(message = "Email Text cannot be null")String text) {
-	
+public class MailBody {
+
+    @NotNull(message = "Recipient Email Address cannot be null")
+    private String to;
+
+    @NotNull(message = "Email Subject cannot be null")
+    private String subject;
+
+    @NotNull(message = "Email Text cannot be null")
+    private String text;
 }
+
