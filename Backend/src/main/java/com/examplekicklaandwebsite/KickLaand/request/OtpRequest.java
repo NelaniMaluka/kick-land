aaf2e97da7b43308bf8cdd4f55e933acabc73ce2
Nelani Maluka <malukanelani@gmail.com>
2025-05-24 11:27:51 +0200

    chore: improved error handling

--- Backend/src/main/java/com/examplekicklaandwebsite/KickLaand/request/OtpRequest.java
+++ Backend/src/main/java/com/examplekicklaandwebsite/KickLaand/request/OtpRequest.java
@@ -2,9 +2,18 @@ package com.examplekicklaandwebsite.KickLaand.request;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Positive;
 
 @Builder
 public record OtpRequest(
-                @NotNull(message = "OTP cannot be null") Integer otp,
-                @NotNull(message = "Email cannot be null") String email) {
+        @NotNull(message = "OTP cannot be null")
+        @Positive(message = "OTP must be a positive number")
+        Integer otp,
+
+        @NotNull(message = "Email cannot be null")
+        @Email(message = "Please enter a valid email address")
+        String email
+) {
 }
+
